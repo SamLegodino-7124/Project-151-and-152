be6b66ccdbc306e5d047307a16524fc13cee38a0
SamLegodino-7124 <83705032+SamLegodino-7124@users.noreply.github.com>
2022-04-07 15:23:24 +0800

    Add files via upload

---
+++ Project152/Box.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent("move-box-on-click", {
+  schema: {
+    moveX: { type: "number", default: 0 },
+  },
+
+  tick: function () {
+    window.addEventListener('click',(e)=>{
+      this.data.moveX = this.data.moveX + 0.005;
+    })
+
+    var pos = this.el.getAttribute("position");
+
+    pos.x = pos.x+this.data.moveY;
+
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+  }
+});
